b53ec72b71cd73ed4ba49eeb8ea3cdc5e467c9c5
badorius <salvador.muntane@gmail.com>
2025-02-10 21:07:05 +0100

    fuzzy

--- ansible/files/fuzzy_lock.sh
+++ ansible/files/fuzzy_lock.sh
@@ -8,7 +8,7 @@ mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
 #convert /tmp/screen_locked.png -blur 0x8 -channel RGBA /tmp/screen_locked.png
 
 # Lock screen displaying this image.
-i3lock -i /tmp/screen_locked.png
+swaylock -i /tmp/screen_locked.png
 
 # Turn the screen off after a delay.
-sleep 60; pgrep i3lock && xset dpms force off
+sleep 60; pgrep swaylock && xset dpms force off
